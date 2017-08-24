6e90f3c1e320a1ee1e5cc82a787fe9fcd48b612b
Michael Maier <maier1michael@gmail.com>
2017-08-24 18:44:11 +0200

    print default app settings

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+import Data.DNAnts.AppSettings
+       (AppSettings(AppSettings, framesPerSecond, gridExtends,
+                    gridSpacing, initTeamSize, numTeams, roundsPerSecond, showGrid,
+                    showTraces, teamCodes, traceRounds))
+
+defaultAppSettings :: AppSettings
+defaultAppSettings =
+  AppSettings
+  { gridExtends = (23, 23)
+  , gridSpacing = 32
+  , framesPerSecond = 30
+  , roundsPerSecond = 4
+  , traceRounds = 300
+  , initTeamSize = 5
+  , numTeams = 0
+  , showGrid = False
+  , showTraces = False
+  , teamCodes = []
+  }
 
 main :: IO ()
-main = someFunc
+main = print $ show defaultAppSettings
