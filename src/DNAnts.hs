50b6844c65af7d20b63580bbebbadbf291541505
Michael Maier <maier1michael@gmail.com>
2017-08-26 16:38:29 +0200

    show window

--- src/DNAnts.hs
+++ src/DNAnts.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module DNAnts
   ( runApp
   ) where
 
-import Data.DNAnts.AppSettings (AppSettings(AppSettings))
+import Control.Exception (finally)
+import DNAnts.View.Window (runWindow)
+import Data.DNAnts.AppSettings
+       (AppSettings(AppSettings, gridExtends, gridSpacing))
+import Data.Text (pack)
+import qualified SDL
+import SDL.Vect (V2(V2))
+
+defer = flip finally
 
 runApp :: String -> AppSettings -> IO ()
-runApp title settings = do
-  print $ "running app: " ++ title
-  print $ show settings
+runApp title settings@AppSettings {gridExtends, gridSpacing} =
+  let (gridWidth, gridHeight) = gridExtends
+      windowWidth = fromIntegral (gridWidth * gridSpacing)
+      windowHeight = fromIntegral (gridHeight * gridSpacing)
+  in runWindow (pack title) windowWidth windowHeight $ \window -> do
+       SDL.delay 3000000
+       SDL.quit
---
+++ src/DNAnts/View/Window.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module DNAnts.View.Window
+  ( runWindow
+  ) where
+
+import Control.Exception (finally)
+import Data.Text (Text)
+import Foreign.C.Types (CInt)
+import qualified SDL
+import SDL.Vect (V2(V2))
+
+data Window = Window
+  { window :: SDL.Window
+  , renderer :: SDL.Renderer
+  }
+
+defer = flip finally
+
+runWindow :: Text -> CInt -> CInt -> (Window -> IO a) -> IO a
+runWindow title width height app = do
+  SDL.initialize [SDL.InitVideo]
+  defer SDL.quit $ do
+    window <-
+      SDL.createWindow
+        title
+        SDL.defaultWindow
+        { SDL.windowPosition = SDL.Centered
+        , SDL.windowInitialSize = V2 width height
+        }
+    defer (SDL.destroyWindow window) $ do
+      SDL.raiseWindow window
+      renderer <-
+        SDL.createRenderer
+          window
+          (-1)
+          SDL.RendererConfig
+          { SDL.rendererType = SDL.AcceleratedVSyncRenderer
+          , SDL.rendererTargetTexture = True
+          }
+      defer (SDL.destroyRenderer renderer) $ app Window {window, renderer}
