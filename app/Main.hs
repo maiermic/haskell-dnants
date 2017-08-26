2ba20150628f8631964af5b541b97cf4746fcb33
Michael Maier <maier1michael@gmail.com>
2017-08-26 11:14:55 +0200

    process arguments

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,10 @@ import Data.DNAnts.AppSettings
        (AppSettings(AppSettings, framesPerSecond, gridExtends,
                     gridSpacing, initTeamSize, numTeams, roundsPerSecond, showGrid,
                     showTraces, teamCodes, traceRounds))
+import System.Console.GetOpt
+       (ArgDescr(NoArg, OptArg, ReqArg), ArgOrder(RequireOrder),
+        OptDescr(Option), getOpt, usageInfo)
+import System.Environment (getArgs, getProgName)
 
 defaultAppSettings :: AppSettings
 defaultAppSettings =
@@ -20,5 +24,72 @@ defaultAppSettings =
   , teamCodes = []
   }
 
+options :: [OptDescr (AppSettings -> IO AppSettings)]
+options =
+  [ Option
+      "g"
+      ["grid"]
+      (ReqArg
+         (\arg settings -> return settings {gridExtends = (read arg, read arg)})
+         "size")
+      "Size of the grid (equal width and height)"
+  , Option
+      "w"
+      ["grid-w"]
+      (ReqArg
+         (\arg settings@AppSettings {gridExtends = (_, height)} ->
+            return settings {gridExtends = (read arg, height)})
+         "width")
+      "Width of the grid"
+  , Option
+      "h"
+      ["grid-h"]
+      (ReqArg
+         (\arg settings@AppSettings {gridExtends = (width, _)} ->
+            return settings {gridExtends = (width, read arg)})
+         "height")
+      "Height of the grid"
+  , Option
+      "r"
+      ["trace-rounds"]
+      (ReqArg
+         (\arg settings -> return settings {traceRounds = read arg})
+         "rounds")
+      "Trace rounds"
+  , Option
+      "t"
+      ["team"]
+      (ReqArg
+         (\arg settings ->
+            return settings {teamCodes = teamCodes settings ++ [arg]})
+         "team-code")
+      "Add a team (multiple teams can be added)"
+  , Option
+      "s"
+      ["team-size"]
+      (ReqArg
+         (\arg settings -> return settings {initTeamSize = read arg})
+         "size")
+      "Set the initial team size"
+  , Option
+      "b"
+      ["big"]
+      (NoArg (\settings -> return settings {gridSpacing = 32}))
+      "Show big grid"
+  ]
+
+-- TODO use_32px
+-- TODO validate that length of teamCodes is at most 4
+processArgs :: [String] -> IO AppSettings
+processArgs args =
+  case getOpt RequireOrder options args of
+    (actions, _, []) -> foldl (>>=) (return defaultAppSettings) actions
+    (_, _, errs) ->
+      ioError (userError (concat errs ++ usageInfo header options))
+  where
+    header = "Usage: dnants [OPTION...]"
+
 main :: IO ()
-main = print $ show defaultAppSettings
+main = do
+  settings <- getArgs >>= processArgs
+  print $ show settings
